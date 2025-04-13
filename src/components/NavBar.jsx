7de2abb84c2cb51c330b91e8f547238ac53a6217
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-13 15:43:05 +0100

    adding some divs,classes, img and links tags

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { assests } from "../assets/assets";
 
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <div className="absolute top-0 left-0 w-full z-10">
+      <div>
+        <img src={assests.logo} alt="logo" />
+        <ul>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Home
+          </a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            About
+          </a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Projects
+          </a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Testimonials
+          </a>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
