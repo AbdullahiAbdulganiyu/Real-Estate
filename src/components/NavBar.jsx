4a6ae287586eb2c70e74042eb2223782e829cd98
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-14 09:41:00 +0100

    fixing some spelling mistakes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { assests } from "../assets/assets";
+import { assets } from "../assets/assets";
 
 const NavBar = () => {
   return (
     <div className="absolute top-0 left-0 w-full z-10">
       <div>
-        <img src={assests.logo} alt="logo" />
+        <img src={assets.logo} alt="logo" />
         <ul>
           <a href="#Header" className="cursor-pointer hover:text-gray-400">
             Home
