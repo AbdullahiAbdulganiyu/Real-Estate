1cd255e4a382548ead9936af7d795aa82bf4cfd2
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-16 09:35:21 +0100

    adding some tailwinds classes to nicely format the h2 text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,9 @@ const Header = () => {
     >
       <NavBar />
       <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white ">
-        <h2>Explore homes that fit your dreams</h2>
+        <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-width-3xl font-semibold pt-20">
+          Explore homes that fit your dreams
+        </h2>
         <div>
           <a href="">Projects</a>
           <a href="">Contact Us</a>
