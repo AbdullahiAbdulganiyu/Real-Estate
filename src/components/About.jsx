1c109e9a3884ca9cf2012f16dc31cd68337d0d5c
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-16 14:25:43 +0100

    adding a class for text size on different screen size

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       className="flex flex-col items-center justify-center container mx-auto p-14 md:px-20  lg:px-32 w-full overflow-hidden"
       id="About"
     >
-      <h1>
+      <h1 className="text-2xl sm:text-4xl">
         About <span>Our Brand</span>
       </h1>
       <p>Passionate About Properties, Dedicated to Your Vision</p>
