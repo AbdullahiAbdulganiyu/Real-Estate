4acaceaf9bf127638261c01a2c991fbc6f8a2a5a
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-16 10:57:40 +0100

    creating a state for showing mobile menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 
 const NavBar = () => {
+  const [showmobileMenu, setShowMobileMenu] = useState(false);
   return (
     <div className="absolute top-0 left-0 w-full z-10">
       <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent ">
