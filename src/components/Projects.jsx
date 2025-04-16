91b2b85615d8212e64f8e16f2fcc23dbb3b2583a
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-16 21:03:02 +0100

    adding a tilte and other informations

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>
+        Projects <span>Completed</span>
+      </h1>
+      <p>Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
+    </div>
+  );
 };
 
 export default Projects;
