4d71856efee691287586aa22bdbc038542b1afa5
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-16 21:09:15 +0100

    adding some classes to nicely format the div

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div>
+    <div className="container mx-auto py-4 pt-20 px-6 md:px-20 lg-px-32 my-20 w-full overflow-hidden">
       <h1>
         Projects <span>Completed</span>
       </h1>
