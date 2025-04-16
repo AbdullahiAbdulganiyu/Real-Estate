130b5137ab5c35a36d37bfaf7cc4de74075609ab
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-16 21:10:17 +0100

    adding id

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div className="container mx-auto py-4 pt-20 px-6 md:px-20 lg-px-32 my-20 w-full overflow-hidden">
+    <div
+      className="container mx-auto py-4 pt-20 px-6 md:px-20 lg-px-32 my-20 w-full overflow-hidden"
+      id="Projects"
+    >
       <h1>
         Projects <span>Completed</span>
       </h1>
