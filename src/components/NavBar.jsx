46407e232e5f223b88f5fa78e3e2a5a8272d348a
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-16 11:20:36 +0100

    adding a useeffect hook that depends on the showmobilemenu state

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 
 const NavBar = () => {
   const [showmobileMenu, setShowMobileMenu] = useState(false);
+
+  useEffect(() => {
+    if (showmobileMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [showmobileMenu]);
   return (
     <div className="absolute top-0 left-0 w-full z-10">
       <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent ">
