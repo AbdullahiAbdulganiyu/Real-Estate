e35aa6fcbb404e8e03029dc24a05896d1ed082dd
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-18 15:16:23 +0100

    adding some id

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Testimonials = () => {
   return (
-    <div className="container mx-auto px-10 lg:px-32 w-full overflow-hidden">
+    <div
+      className="container mx-auto px-10 lg:px-32 w-full overflow-hidden"
+      id="Testimonials"
+    >
       <h1>
         Customers <span>Testimonials</span>
       </h1>
