8050c6a7c9bdb27bb21ee2c4ca829a4d21dac07e
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-18 15:10:55 +0100

    creating the testimonials component and adding some contents to it

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div>
+      <h1>
+        Customers <span>Testimonials</span>
+      </h1>
+      <p>Real Stories from Those who found Home with Us</p>
+    </div>
+  );
+};
+
+export default Testimonials;
