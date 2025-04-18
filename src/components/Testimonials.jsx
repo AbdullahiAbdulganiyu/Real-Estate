a6f686ae1353973c4d6551d52328f36fdb0e2f77
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-18 15:22:53 +0100

    adding some classes to nicely format the header and span

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,8 +6,11 @@ const Testimonials = () => {
       className="container mx-auto px-10 lg:px-32 w-full overflow-hidden"
       id="Testimonials"
     >
-      <h1>
-        Customers <span>Testimonials</span>
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Customers{" "}
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          Testimonials
+        </span>
       </h1>
       <p>Real Stories from Those who found Home with Us</p>
     </div>
