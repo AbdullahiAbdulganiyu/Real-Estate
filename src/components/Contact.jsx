bc4da29137fd4da4db05ea5879e90ec7b5b5012b
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-19 16:58:06 +0100

    fixing some ,istakes in the code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,33 +1,30 @@
 import React from "react";
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
 
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
 
-    function App() {
-        const [result, setResult] = React.useState("");
-      
-        const onSubmit = async (event) => {
-          event.preventDefault();
-          setResult("Sending....");
-          const formData = new FormData(event.target);
-      
-          formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
-      
-          const response = await fetch("https://api.web3forms.com/submit", {
-            method: "POST",
-            body: formData
-          });
-      
-          const data = await response.json();
-      
-          if (data.success) {
-            setResult("Form Submitted Successfully");
-            event.target.reset();
-          } else {
-            console.log("Error", data);
-            setResult(data.message);
-          }
-        };
+    formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
 
   return (
     <div
