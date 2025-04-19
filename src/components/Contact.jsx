2e48db34f92897984eb0ee19030fd8f920f0af0b
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-19 16:56:33 +0100

    creating a contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 
 const Contact = () => {
+
+
+    function App() {
+        const [result, setResult] = React.useState("");
+      
+        const onSubmit = async (event) => {
+          event.preventDefault();
+          setResult("Sending....");
+          const formData = new FormData(event.target);
+      
+          formData.append("access_key", "YOUR_ACCESS_KEY_HERE");
+      
+          const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+          });
+      
+          const data = await response.json();
+      
+          if (data.success) {
+            setResult("Form Submitted Successfully");
+            event.target.reset();
+          } else {
+            console.log("Error", data);
+            setResult(data.message);
+          }
+        };
+
   return (
     <div
       className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
