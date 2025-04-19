b84ffddb88225e4ec8aed465ec50e14764161e48
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-19 13:39:23 +0100

    adding an id

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"></div>
+    <div
+      className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
+      id="Contact"
+    ></div>
   );
 };
 
