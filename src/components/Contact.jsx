8dc0976bc6d3ceabf4f3a1db92c7328424439c1f
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-19 13:43:37 +0100

    updating the informations of the the contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,17 @@ const Contact = () => {
     <div
       className="text-center p-6 py-20 lg:px-32 w-full overflow-hidden"
       id="Contact"
-    ></div>
+    >
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Contact
+        <span className="underline underline-offset-4 decoration-1 under font-light">
+          With Us
+        </span>
+      </h1>
+      <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
+        Ready to Make a Move?, Let's Build Your Feature Together
+      </p>
+    </div>
   );
 };
 
