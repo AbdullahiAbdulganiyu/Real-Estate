5e15d6d2d654da5394366ad377154cb0734f174c
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-19 17:59:18 +0100

    fixing mistake in some class name

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
             industry. Lorem Ipsum has been the industry's standard dummy text.
           </p>
         </div>
-        <div className="w-full md:1/5 mb-8 md:mb-0">
+        <div className="w-full md:w-1/5 mb-8 md:mb-0">
           <h3 className="text-white text-lg font-bold mb-4">Company</h3>
           <ul className="flex flex-col gap-2 text-gray-400 ">
             <a href="#Header" className="hover:text-white">
