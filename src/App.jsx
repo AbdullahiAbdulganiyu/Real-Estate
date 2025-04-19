c91e3ba17af7dd6e275b65aa9e3511a504eba67c
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-19 17:13:54 +0100

    importing toastcontainer from react-toastify

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
