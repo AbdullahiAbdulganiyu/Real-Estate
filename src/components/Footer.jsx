ff4b1fd91f5921ab3850051753046ba0c95c61c7
AbdullahiAbdulganiyu <ludba020@gmail.com>
2025-04-20 09:31:36 +0100

    adding some classes to nicely format the div for the copyright text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -53,7 +53,9 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div>Copyright {year} ©️ GreatStack. All Right Reserved</div>
+      <div className="border-t border-gray-700 py-4 mt-10 text-center text-gray-500">
+        Copyright {year} ©️ GreatStack. All Right Reserved
+      </div>
     </div>
   );
 };
